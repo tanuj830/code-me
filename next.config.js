4b39bd1a3e1bfd3131b23dd8f7e21ecc1b0ec867
tanuj <tanujbhatt644@gmail.com>
2023-10-26 17:04:45 +0530

    bug fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // eslint-disable-next-line space-before-blocks
+    if (isServer) {
+      config.externals = nodeExternals();
+    }
 
+    return config;
+  },
+};
 module.exports = {
   images: {
     domains: ["https://www.freepik.com/"],
